95c3299f2fdec12d5f2550f6fec0c046461abf76
Tkhao Lanh <98495637+TkhaoLanh@users.noreply.github.com>
2023-10-08 22:47:12 -0400

    Create a story board for item management

---
+++ AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Assignment1
+//
+//  Created by user248619 on 9/28/23.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
